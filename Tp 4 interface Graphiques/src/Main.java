8e39a8eab67659708e0655edbca65ffab174b707
Abderrahmane TAHIRI <abderrahmane.tahiri.02@gmail.com>
2023-10-31 11:23:39 +0100

    Adding local variable in the main fnction

--- Tp 4 interface Graphiques/src/Main.java
+++ Tp 4 interface Graphiques/src/Main.java
@@ -1,12 +1,6 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
-
 public class Main {
     public static void main(String[] args) {
-        new InformationEtudiant();
+
+        InformationEtudiant informationEtudiant = new InformationEtudiant();
     }
 }
